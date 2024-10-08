2734652298109a12598eb0782e3c2bbf24192b74
zdellaert <zoedellaert@gmail.com>
2024-10-07 21:36:02 -0400

    script update

--- scripts/GeneExt.sh
+++ scripts/GeneExt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 120:00:00
 #SBATCH --nodes=1 --ntasks-per-node=20
-#SBATCH --mem=200GB
+#SBATCH --mem=50GB
 #SBATCH --export=NONE
 #SBATCH --error=../scripts/outs_errs/"%x_error.%j" #write out slurm error reports
 #SBATCH --output=../scripts/outs_errs/"%x_output.%j" #write out any program outpus
@@ -19,13 +19,13 @@ source ~/.bashrc
 module load Miniconda3/4.9.2
 conda activate geneext
 
-mkdir geneext
+mkdir -p geneext
 cd geneext
 
 # use --clip_strand both to not allow GeneExt to create overlaps on the same strand
 
-python /data/putnamlab/zdellaert/GeneExt/geneext.py --verbose=3 \
-    -g ../references/Pocillopora_acuta_HIv2_modified.gtf \
+python /data/putnamlab/conda/GeneExt/geneext.py --verbose=3 \
+    -g ../Pocillopora_acuta_HIv2_modified.gtf \
     -b ../../output_RNA/hisat2/merge.bam \
     -o Pocillopora_acuta_GeneExt.gtf \
     -j 18 --clip_strand both
