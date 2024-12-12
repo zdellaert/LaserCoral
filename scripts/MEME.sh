db21b5f731ed73dfc73c4643f2a1700f46b593cf
zdellaert <zdellaert@uri.edu>
2024-12-12 12:00:16 +0000

    Meme output

--- scripts/MEME.sh
+++ scripts/MEME.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --export=NONE
-#SBATCH --nodes=1 --ntasks-per-node=48
+#SBATCH --nodes=1 --ntasks-per-node=12
 #SBATCH --signal=2
 #SBATCH --no-requeue
-#SBATCH --mem=500GB
-#SBATCH --error="%x_error.%j" 
-#SBATCH --output="%x_output.%j" 
+#SBATCH --mem=100GB
+#SBATCH --error=../scripts/outs_errs/"%x_error.%j"
+#SBATCH --output=../scripts/outs_errs/"%x_output.%j"
 #SBATCH -t 24:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL #email you when job starts, stops and/or fails
 
@@ -15,4 +15,7 @@ cd ../output_RNA/differential_expression/TFs
 
 module load apptainer/latest
 
-singularity exec --cleanenv --debug $SINGULARITY_IMAGE meme promoters_500_upstream.fasta -maxw 25 -mod anr -nmotifs 2 -oc meme_output
+singularity exec --cleanenv $SINGULARITY_IMAGE meme promoters_500_upstream_upAboral.fasta -dna -maxw 25 -mod anr -nmotifs 10 -p 6 -oc meme_output_upAboral -revcomp
+
+singularity exec --cleanenv $SINGULARITY_IMAGE meme promoters_500_upstream_upOralEpi.fasta -dna -maxw 25 -mod anr -nmotifs 10 -p 6 -oc meme_output_upOralEpi -revcomp
+
