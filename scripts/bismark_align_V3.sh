56cf8fa17a6b9ec9efb2ba677eecd1bf2a03f9e7
zdellaert <zdellaert@uri.edu>
2025-01-23 16:44:52 +0000

    bismark full results v3

---
+++ scripts/bismark_align_V3.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+#SBATCH --ntasks=1 --cpus-per-task=30 #split one task over multiple CPU
+#SBATCH --array=0-9 #for 10 samples
+#SBATCH --mem=100GB
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80 #email you when job stops and/or fails or is nearing its time limit
+#SBATCH --error=scripts/outs_errs/"%x_error.%j" #if your job fails, the error report will be put in this file
+#SBATCH --output=scripts/outs_errs/"%x_output.%j" #once your job is completed, any final job report comments will be put in this file
+#SBATCH -D /project/pi_hputnam_uri_edu/zdellaert/LaserCoral
+
+# load modules needed
+module load uri/main
+module load Bismark/0.23.1-foss-2021b
+module load bowtie2/2.5.2
+
+# Set directories and files
+reads_dir="data_WGBS/" #directory containing trimmed data to align
+genome_folder="references/" #directory containing original unmodified genome fasta and bismark Bisulfite_Genome directory
+
+mkdir -p output_WGBS/align_V3 #make output directory if it does not exist
+
+output_dir="output_WGBS/align_V3"
+checkpoint_file="output_WGBS/align_V3/completed_samples.log"
+
+# Create the checkpoint file if it doesn't exist
+touch ${checkpoint_file}
+
+# Get the list of sample files and corresponding sample names
+files=(${reads_dir}trimmed_V3_LCM_*R1_001.fastq.gz)
+file="${files[$SLURM_ARRAY_TASK_ID]}"
+sample_name=$(basename "$file" "_R1_001.fastq.gz")
+
+# Check if the sample has already been processed
+if grep -q "^${sample_name}$" ${checkpoint_file}; then
+    echo "Sample ${sample_name} already processed. Skipping..."
+    exit 0
+fi
+
+# Define log files for stdout and stderr
+stdout_log="${output_dir}${sample_name}_stdout.log"
+stderr_log="${output_dir}${sample_name}_stderr.log"
+
+# Run Bismark align
+bismark \
+    -genome ${genome_folder} \
+    -p 8 \
+    -score_min L,0,-1.0 \
+    -1 ${reads_dir}${sample_name}_R1_001.fastq.gz \
+    -2 ${reads_dir}${sample_name}_R2_001.fastq.gz \
+    -o ${output_dir} \
+    --basename ${sample_name} \
+    2> "${output_dir}/${sample_name}-bismark_summary.txt"
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    # Append the sample name to the checkpoint file
+    echo ${sample_name} >> ${checkpoint_file}
+    echo "Sample ${sample_name} processed successfully."
+else
+    echo "Sample ${sample_name} failed. Check ${stderr_log} for details."
+fi
+
+# Define directories
+summary_file="${output_dir}/parameter_comparison_summary.csv"
+
+# Initialize summary file
+echo "Sample,Score_Min,Alignment_Rate" > ${summary_file}
+
+for file in ${output_dir}/*_report.txt; do
+    # Extract sample name and from directory name
+    sample_name=$(basename "$file" | cut -d'_' -f3-4)
+    score_min="L0-1.0"
+
+    # Locate the summary file
+    summary_file_path="${output_dir}/${sample_name}_PE_report.txt"
+
+    # Extract metrics
+    mapping=$(grep "Mapping efficiency:" ${summary_file_path} | awk '{gsub("%", "", $3); print $3}')
+
+    # Append to the summary file
+    echo "${sample_name},${score_min},${mapping}" >> ${summary_file}
+done
