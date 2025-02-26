ae75ae9320363a534dbfe09ed363ab9ebc37abbb
zdellaert <zdellaert@uri.edu>
2025-02-26 15:32:22 +0000

    unity up to date

---
+++ scripts/spladder_build_ext.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --ntasks=1 --cpus-per-task=6 #split one task over multiple CPU
+#SBATCH --mem=250GB
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80 #email you when job stops and/or fails or is nearing its time limit
+#SBATCH --error=scripts/outs_errs/"%x_error.%j" #if your job fails, the error report will be put in this file
+#SBATCH --output=scripts/outs_errs/"%x_output.%j" #once your job is completed, any final job report comments will be put in this file
+#SBATCH -D /project/pi_hputnam_uri_edu/zdellaert/LaserCoral
+
+module load conda/latest
+conda activate /work/pi_hputnam_uri_edu/conda/envs/spladder
+
+spladder_dir="output_RNA/splicing"
+out_dir="/scratch3/workspace/zdellaert_uri_edu-shared/spladder_out_ext"
+
+mkdir -p ${out_dir}
+
+spladder build --bams ${spladder_dir}/alignments.txt \
+               --annotation ${spladder_dir}/Pocillopora_acuta_GeneExt.gtf \
+               --outdir ${out_dir} \
+               --parallel 4 \
+               --verbose
---
+++ scripts/spladder_test_ext.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --ntasks=1 --cpus-per-task=12 #split one task over multiple CPU
+#SBATCH --mem=250GB
+#SBATCH -t 48:00:00
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_80 #email you when job stops and/or fails or is nearing its time limit
+#SBATCH --error=scripts/outs_errs/"%x_error.%j" #if your job fails, the error report will be put in this file
+#SBATCH --output=scripts/outs_errs/"%x_output.%j" #once your job is completed, any final job report comments will be put in this file
+#SBATCH -D /project/pi_hputnam_uri_edu/zdellaert/LaserCoral
+
+module load conda/latest
+conda activate /work/pi_hputnam_uri_edu/conda/envs/spladder
+
+spladder_dir="output_RNA/splicing"
+out_dir="/scratch3/workspace/zdellaert_uri_edu-shared/spladder_out_ext"
+
+spladder test --conditionA ${spladder_dir}/Oral.txt \
+              --conditionB ${spladder_dir}/Aboral.txt \
+              --labelA Oral --labelB Aboral \
+              --diagnose-plots \
+              --parallel 12 \
+              --outdir ${out_dir}
